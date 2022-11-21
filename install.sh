344a23fe1f19a89f951f67356b5d544d90d571b1
Daniel <43763882+aspectiscool@users.noreply.github.com>
2022-11-21 02:31:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o Hydrogen_MacOS.zip https://cdn.hydrogen.sh/hydrogen-macos.zip
+curl -o Hydrogen_MacOS.zip https://cdn.hydrogen.sh/Hydrogen_MacOS.zip
 unzip -qq -o Hydrogen_MacOS.zip > /dev/null 2>&1
 rm -r /Applications/Hydrogen.app > /dev/null 2>&1
 mv "Hydrogen MacOS.app" /Applications/Hydrogen.app
