1667329d9c01af7ae2f1d54d2bbf430f5f60b5c7
Braeden <braeden.bw@gmail.com>
2022-12-02 12:07:16 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ unzip -qq -o Hydrogen_MacOS.zip > /dev/null 2>&1
 rm -r /Applications/Hydrogen.app > /dev/null 2>&1
 mv "Hydrogen MacOS.app" /Applications/Hydrogen.app
 rm Hydrogen_MacOS.zip
+mkdir /Applications/Roblox.app/Contents/Resources/Hydrogen
 echo Hydrogen has been installed!
