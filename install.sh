735eb3523ea0b083547a2431df4ca9cba1d1d696
Braeden <braeden.bw@gmail.com>
2022-12-02 22:19:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
 printf "\n!!! IF YOU SEE ANY Operation not permitted ERRORS, PLEASE VIEW THE VIDEO AT https://www.youtube.com/watch\?v\=gDSM8SqDatc !!!\033[0m\n\n"
 curl -o Hydrogen_MacOS.zip https://cdn.discordapp.com/attachments/1043972790266626179/1048442240659361903/Hydrogen_MacOS.zip
-unzip -qq -o Hydrogen_MacOS.zip > /dev/null 2>&1
-rm -r /Applications/Hydrogen.app > /dev/null 2>&1
+unzip -qq -o Hydrogen_MacOS.zip
+rm -r /Applications/Hydrogen.app
 mv "Hydrogen MacOS.app" /Applications/Hydrogen.app
 rm Hydrogen_MacOS.zip
 cp /Applications/Hydrogen.app/Contents/Resources/libHydrogen.dylib /Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib
