4aaeecfd1eb9127e9bcac07bc4d8e1ddcec17f52
xgladius <stormwizard1001@gmail.com>
2023-03-08 00:02:56 -0600

    initial

---
+++ install.sh
@@ -0,0 +1,20 @@
+ #!/bin/zsh
+printf "\n"
+printf "\e[31m\n"
+printf "IF YOU ARE GETTING ERRORS DURING INSTALL PLEASE VIEW THIS VIDEO: https://youtu.be/gDSM8SqDatc\n"
+printf "If you are still getting errors after that, run the install command with 'sudo' before it\n"
+printf "\033[0m\n"
+printf "\n"
+curl -o Hydrogen_MacOS.zip https://cdn.discordapp.com/attachments/1043972790266626179/1082905716643336282/Hydrogen_MacOS.zip
+unzip -o -qq Hydrogen_MacOS.zip
+rm -rf /Applications/Hydrogen.app
+mv "Hydrogen MacOS.app" /Applications/Hydrogen.app
+rm Hydrogen_MacOS.zip
+cp /Applications/Roblox.app/Contents/MacOS/RobloxPlayerBack /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
+cp /Applications/Hydrogen.app/Contents/Resources/libHydrogen.dylib /Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib
+cp /Applications/Roblox.app/Contents/MacOS/RobloxPlayer /Applications/Roblox.app/Contents/MacOS/RobloxPlayerBack
+/Applications/Hydrogen.app/Contents/Resources/insert_dylib --strip-codesig --all-yes /Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib /Applications/Roblox.app/Contents/MacOS/RobloxPlayerBack /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
+mkdir /Users/Shared/Hydrogen
+mkdir /Users/Shared/Hydrogen/scripts
+mkdir /Users/Shared/Hydrogen/autoexec
+echo Hydrogen has been installed!
